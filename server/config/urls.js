4e133013d7c368b9d2197c8b82dc4c9109e1030f
Cary Meskell <carymeskell@gmail.com>
2024-10-18 21:45:20 -0700

    db auth from ENV

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,9 +1,11 @@
+const DB_AUTH = require('./dbAuth.js');
+
 const DIALECT = 'mongodb';//'postgres';
 const HOST = 'localhost';
 // const PORT = 5432;
 const DB = 'user';
-// const DB_USR = require('./dbAuth.js').DB_USR;
-// const DB_PASS = require('./dbAuth.js').DB_PASS;
+const DB_USR = process.env.DB_USR || DB_AUTH.DB_USR;
+const DB_PASS = process.env.DB_PASS || DB_AUTH.DB_PASS;
 
 module.exports = {
   default: 3001,
