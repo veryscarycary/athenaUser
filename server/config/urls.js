3acad58b4e44827639cc33131f02d9cf2e118ba9
Cary Meskell <carymeskell@gmail.com>
2024-10-19 23:55:24 -0700

    remove dbAuth, use environment instead

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,11 +1,9 @@
-const DB_AUTH = require('./dbAuth.js');
-
 const DIALECT = 'mongodb';//'postgres';
 const HOST = 'localhost';
 // const PORT = 5432;
 const DB = 'user';
-const DB_USR = process.env.DB_USR || DB_AUTH.DB_USR;
-const DB_PASS = process.env.DB_PASS || DB_AUTH.DB_PASS;
+const DB_USR = process.env.POSTGRES_USER;
+const DB_PASS = process.env.POSTGRES_PASSWORD;
 
 module.exports = {
   default: 3001,
