96d27973d28336b3e0e15cef9b35d652d8266dcf
Cary Meskell <carymeskell@gmail.com>
2024-10-20 00:44:00 -0700

    add hostname switch depending on dockercompose or not

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,5 +1,7 @@
-const DIALECT = 'mongodb';//'postgres';
-const HOST = 'localhost';
+const DIALECT = 'mongodb'; //'postgres';
+const HOST = process.env.DOCKER_COMPOSE
+    ? 'mongodb'
+    : 'localhost';
 // const PORT = 5432;
 const DB = 'user';
 const DB_USR = process.env.POSTGRES_USER;
@@ -7,7 +9,7 @@ const DB_PASS = process.env.POSTGRES_PASSWORD;
 
 module.exports = {
   default: 3001,
-  database: `${DIALECT}://${HOST}/${DB}`
+  database: `${DIALECT}://${HOST}/${DB}`,
   // database: `${DIALECT}://${DB_USR}:${DB_PASS}@${HOST}:${PORT}/${DB}`,
   // databaseDefault: `${DIALECT}://${DB_USR}:${DB_PASS}@${HOST}:${PORT}/${DIALECT}`
 };
